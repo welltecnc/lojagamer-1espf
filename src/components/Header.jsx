678644034665cfd9b44f441ae7fdec0fdd5ae1af
Wellington <welltecnic@hotmail.com>
2026-09-25 09:55:08 -0300

    style: Estilizando o Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
-
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <>
-      
-    </>
+    <header className="flex justify-between items-center py-6 px-[5%] bg-black">
+      <h1 className="logo p-2 text-[2rem] text-white font-bold cursor-pointer">
+        LOJA<span className="text-[#95ff00]">GAMER</span></h1>
+      <nav>
+        <ul className="flex list-none items-center gap-8">
+          <li><Link to="/"className=" text-white text-lg no-underline hover:text-[#95ff00] hover:uppercase">Home</Link></li>
+          <li><Link to="/jogos" className=" text-white text-lg no-underline hover:text-[#95ff00] hover:uppercase">Jogos</Link></li>
+          <li><Link to="/contato" className=" text-white text-lg no-underline hover:text-[#95ff00] hover:uppercase">Contato</Link></li>
+          <li><Link to="/login" className=" text-white text-lg no-underline hover:text-[#95ff00] hover:uppercase">Login</Link></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
